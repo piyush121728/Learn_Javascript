7c264c732ff666d4b111710afde52355dbeeb8b6
Piyush Kumar <98932297+piyush121728@users.noreply.github.com>
2023-10-24 06:22:50 +0000

    first project is completed.

--- 07_projects/01-colorChanger/script.js
+++ 07_projects/01-colorChanger/script.js
@@ -1,11 +1,25 @@
-const buttons = document.querySelectorAll('.button');
-// console.log(buttons);
-const body = document.querySelector('body');
-// console.log(body);
+const buttons = document.querySelectorAll('.button'); //this will give the nodeList of all the elements whose calss name is button
+console.log(buttons);
+const body = document.querySelector('body'); 
+console.log(body);
 
 buttons.forEach((button) => {
     console.log(button);
-
-    
-
+    button.addEventListener('click', (e) => {
+        console.log(e);
+        console.log(e.target.id);
+        if (e.target.id === 'grey') {
+            // body.style.backgroundColor = e.target.id;
+            body.setAttribute('style', `background : ${e.target.id}`)
+        }
+        if (e.target.id === 'yellow') {
+            body.setAttribute('style', `background : ${e.target.id}`)
+        }
+        if (e.target.id === 'blue') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'white') {
+            body.style.backgroundColor = e.target.id;
+        }
+    })
 })
