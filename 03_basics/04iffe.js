bdf0b4e693f9fa91ef1f5c2b575234466fe84681
Piyush Kumar <98932297+piyush121728@users.noreply.github.com>
2023-10-25 05:54:32 +0530

    Update 04iife.js

--- 03_basics/04iffe.js
+++ 03_basics/04iffe.js
@@ -1,16 +1,16 @@
 /*
 iffe : Immediately Invoked Function Expression
-Sometime problem occur due to pollution of global scope to avoid this pollution we use iffe.
+Sometime problem occur due to pollution of global scope to avoid this pollution we use iife.
 
-Declaration of iffe : 
+Declaration of iife : 
 Synatx :  (function declaration and definition)();
-Note : must use semicolon o.w iffe will not stop the context
+Note : must use semicolon o.w iife will not stop the context
 */
 
 
 
 //named iffe
-(function chai() { console.log('Chai is ready.') })();
+( function chai() { console.log('Chai is ready.') })();
 
 //simple iffe
 ((username)=>{console.log(`user is ${username}`)})('piyush');
